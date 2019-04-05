74c52490148585ec0ba41d6f700a72b2f4d68875
Julia2506 <zbs666x2@gmail.com>
2019-04-05 11:45:52 +0300

    Главная страница

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $pageConfig = [
     'title'=>'Главная страница',
     'cssFiles'=>[
-        '/css/style.css'
+        '/css/style.css',
+        '/css/main.css'
     ],
     'jsFiles'=>[
         '/js/script.js'
@@ -10,6 +11,9 @@ $pageConfig = [
 ];
 include($_SERVER['DOCUMENT_ROOT'].'/parts/header.php');
 ?>
+<h1>Новые поступления весны</h1>
+<p>Мы подобрали для Вас лучшие новинки сезона</p>
+
 
 <?php 
     include($_SERVER['DOCUMENT_ROOT'].'/parts/footer.php');
